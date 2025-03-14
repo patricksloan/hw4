29dd580acb50a01c85fed138d1db9cdbcf106982
Patrick Sloan <patrick.sloan@kellogg.northwestern.edu>
2025-03-14 03:40:45 +0000

    user stories done. file attachment done. need to do bootstrap and deployment.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,18 +1,25 @@
 class EntriesController < ApplicationController
-
   def new
-    @user = User.find_by({ "id" => session["user_id"] }) 
+    @user = User.find_by({ "id" => session["user_id"] })
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = session["user_id"]
-    @entry.save
+    @user = User.find_by({ "id" => session["user_id"] })
+    
+    if @user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = @user["id"]
+      @entry.uploaded_image.attach(params["uploaded_image"]) 
+      @entry.save
+    else
+      flash["notice"] = "Login first."
+    end
+
     redirect_to "/places/#{@entry["place_id"]}"
   end
-
 end
+
